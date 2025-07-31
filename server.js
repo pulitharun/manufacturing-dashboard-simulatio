0dfd5737a9a0e428e646c4fb1642e37178a8c238
Puli <pulitharun21@gmail.com>
2025-07-31 11:58:53 +0100

    Add Server.js to serve the index.html for Node.js dashboard

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading index.html');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
